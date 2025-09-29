c7fc35cf6c6f48872ecb81041a01bae3e6755a79
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-09-29 12:06:06 -0500

    feat(controller): implement create endpoint for InventoryItem

--- src/main/java/com/elara/app/inventory_service/controller/InventoryItemController.java
+++ src/main/java/com/elara/app/inventory_service/controller/InventoryItemController.java
@@ -1,9 +1,17 @@
 package com.elara.app.inventory_service.controller;
 
+import com.elara.app.inventory_service.dto.request.InventoryItemRequest;
+import com.elara.app.inventory_service.dto.response.InventoryItemResponse;
+import com.elara.app.inventory_service.service.interfaces.InventoryItemService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,4 +22,22 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 // @Tag(
 public class InventoryItemController {
+
+    private final InventoryItemService service;
+    private static final String NOMENCLATURE = "InventoryItem-controller";
+
+    // ========================================
+    // CREATE OPERATIONS
+    // ========================================
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<InventoryItemResponse> create(
+        @Valid @RequestBody InventoryItemRequest request
+    ) {
+        log.info("[" + NOMENCLATURE + "-create] Request to create InventoryItem: {}", request);
+        InventoryItemResponse response = service.save(request);
+        log.info("[" + NOMENCLATURE + "-create] InventoryItem created with id: {}", response.id());
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
 }
