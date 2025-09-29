015043f9a16759f4c9a910609214d2b8e9857d86
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2025-09-29 13:27:47 -0500

    fix(service): correct typo in debug log message for findAll method

--- src/main/java/com/elara/app/inventory_service/service/imp/InventoryItemImp.java
+++ src/main/java/com/elara/app/inventory_service/service/imp/InventoryItemImp.java
@@ -92,7 +92,7 @@ public class InventoryItemImp implements InventoryItemService {
     @Override
     public Page<InventoryItemResponse> findAll(Pageable pageable) {
         String methodNomenclature = NOMENCLATURE + "-findAll";
-        log.debug("[{}] Fetching all {} entieies", methodNomenclature, ENTITY_NAME);
+        log.debug("[{}] Fetching all {} entities", methodNomenclature, ENTITY_NAME);
         Page<InventoryItemResponse> page = repository.findAll(pageable).map(mapper::toResponse);
         log.debug("[{}] Fetched {} entities, page size: {}", methodNomenclature, ENTITY_NAME, page.getNumberOfElements());
         return page;
