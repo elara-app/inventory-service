06007e5655c097b99e43270106e0c0066d93d8f5
Andrés Bermúdez <91708539+andresbetov@users.noreply.github.com>
2026-03-28 13:04:51 -0500

    refactor(errors): simplify custom exception constructors to accept a single message

--- src/main/java/com/elara/app/inventory_service/exceptions/ResourceConflictException.java
+++ src/main/java/com/elara/app/inventory_service/exceptions/ResourceConflictException.java
@@ -3,7 +3,7 @@ package com.elara.app.inventory_service.exceptions;
 import com.elara.app.inventory_service.utils.ErrorCode;
 
 public class ResourceConflictException extends BaseException {
-    public ResourceConflictException(Object... args) {
-        super(ErrorCode.RESOURCE_CONFLICT, args);
+    public ResourceConflictException(String message) {
+        super(ErrorCode.RESOURCE_CONFLICT, message);
     }
 }
